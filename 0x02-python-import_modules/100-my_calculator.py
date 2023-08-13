f78fd46864e8efbf9b4443ef7dc0f9aafa6acda8
Fredah14 <fredah.jr@hotmail.com>
2023-08-13 06:14:53 -0700

    6. Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,20 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 if __name__ == "__main__":
     import sys
 
-    num_arguments = len(sys.argv) - 1
-    if num_arguments != 3:
+    from calculator_1 import add, sub, mul, div
+
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
+    a = int(sys.argv[1])
     operator = sys.argv[2]
-        if operator == "+":
+    b = int(sys.argv[3])
+    result = None
+
+    if operator == "+":
         result = add(a, b)
     elif operator == "-":
         result = sub(a, b)
@@ -16,18 +22,6 @@ if __name__ == "__main__":
         result = mul(a, b)
     elif operator == "/":
         result = div(a, b)
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    from calculator_1 import add, sub, mul, div
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-
-    if operator == '+':
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif operator == '-':
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif operator == '*':
-        print("{} * {} = {}".format(a, b, mul(a, b)))
     else:
-        print("{} / {} = {}".format(a, b, div(a, b)))
+        print("Unknown operator. Available operators: +, -, * and /"\)
+        sys.exit(1)

9a0bf89d407908f4c92c7cbb09f4f400a078ccf4
Fredah14 <fredah.jr@hotmail.com>
2023-08-15 12:59:03 -0700

    11. Delete at

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -15,6 +15,6 @@ if __name__ == "__main__":
 
     my_list = [1, 2, 3, 4, 5]
     idx = 3
-    new_list = delete_at(my_list, idx)
+    new_list = delete_at(my_list.copy(), idx)
     print(new_list)
     print(my_list)
